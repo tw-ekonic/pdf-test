969a241ad0595351ccf0643b3774a48848ca9104
Tom Weber <tw@ekonic.de>
2021-08-24 14:50:27 +0200

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="pdf-container">
+    <div class="pdf-list-container" v-if="$data.pdf">
+      <div class="pdf-hover" v-for="i in getLength" :key="i" >
+        <button v-on:click="removePage(i)" class="delete">Delete</button>
+        <pdf class="pdf"
+             :page="i"
+             :src="renderPdf()">
+        </pdf>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+import {PDFDocument} from 'pdf-lib'
+
+import pdf from 'vue-pdf';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  methods: {
+    async removePage(i) {
+      await this.$data.pdf.removePage(i -  1);
+      this.$data.pdf = await this.$data.pdf.saveAsBase64({dataUri: true});
+      console.log(i);
+      this.$data.pdf = await this.$data.pdf.saveAsBase64({dataUri: true});
+    }
+  },
+  computed: {
+    getLength() {
+      console.log(this.$data.pdf);
+      return this.$data.pdf.getPages().length;
+    },
+    async renderPdf() {
+      return await this.$data.pdf.saveAsBase64({dataUri: true});
+    }
+  },
+  data() {
+    return {
+      pdf: undefined
+    }
+  },
+  async mounted() {
+    const url = 'test.pdf'
+    const arrayBuffer = await fetch(url).then(res => res.arrayBuffer())
+    this.$data.pdf = await PDFDocument.load(arrayBuffer);
+  },
+  components: {
+    pdf
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+ .pdf-list-container {
+   display: flex;
+   flex-direction: row;
+   flex-wrap: wrap;
+   width: 100%;
+   justify-content: center;
+ }
+ .pdf {
+   margin: 40px;
+   width: 200px;
+ }
+ .pdf-hover:hover {
+   position: relative;
+   background: #EBEBEB;
+ }
+ .delete {
+   position: absolute;
+   left: 10px;
+   top: 10px;
+ }
 </style>
