5cd3c31569529f97e9a79dcd86e0dffd204ace55
Jens Plüddemann <jens@plueddemann.de>
2021-08-25 09:25:36 +0200

    working pdf deletion

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,78 +1,76 @@
 <template>
   <div class="pdf-container">
     <div class="pdf-list-container" v-if="$data.pdf">
-      <div class="pdf-hover" v-for="i in getLength" :key="i" >
+      <div class="pdf-hover" v-for="i in $data.pages" :key="i">
         <button v-on:click="removePage(i)" class="delete">Delete</button>
-        <pdf class="pdf"
-             :page="i"
-             :src="renderPdf()">
-        </pdf>
+        <pdf class="pdf" :src="$data.renderedPdf" :page="i"> </pdf>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-import {PDFDocument} from 'pdf-lib'
+import { PDFDocument } from 'pdf-lib';
 
 import pdf from 'vue-pdf';
 
 export default {
   name: 'HelloWorld',
-  methods: {
-    async removePage(i) {
-      await this.$data.pdf.removePage(i -  1);
-      this.$data.pdf = await this.$data.pdf.saveAsBase64({dataUri: true});
-      console.log(i);
-      this.$data.pdf = await this.$data.pdf.saveAsBase64({dataUri: true});
-    }
-  },
-  computed: {
-    getLength() {
-      console.log(this.$data.pdf);
-      return this.$data.pdf.getPages().length;
-    },
-    async renderPdf() {
-      return await this.$data.pdf.saveAsBase64({dataUri: true});
-    }
-  },
   data() {
     return {
-      pdf: undefined
-    }
+      pdf: undefined,
+      renderedPdf: String,
+      pages: Number,
+    };
   },
   async mounted() {
-    const url = 'test.pdf'
-    const arrayBuffer = await fetch(url).then(res => res.arrayBuffer())
-    this.$data.pdf = await PDFDocument.load(arrayBuffer);
+    const url = 'test.pdf';
+    const arrayBuffer = await fetch(url).then((res) => res.arrayBuffer());
+    const doc = await PDFDocument.load(arrayBuffer);
+
+    this.$data.pdf = doc;
+    this.$data.renderedPdf = await doc.save();
+    this.pages = doc.getPageCount();
+  },
+  methods: {
+    async removePage(i) {
+      if (i > 0 && this.$data.pages > i) {
+        this.$data.pdf.removePage(i - 1);
+        this.$nextTick(async () => {
+          const rawPdf = await this.pdf.save();
+          const doc = await PDFDocument.load(rawPdf);
+          this.renderedPdf = rawPdf;
+          this.pages = doc.getPageCount();
+        });
+      }
+    },
   },
   components: {
-    pdf
-  }
-}
+    pdf,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- .pdf-list-container {
-   display: flex;
-   flex-direction: row;
-   flex-wrap: wrap;
-   width: 100%;
-   justify-content: center;
- }
- .pdf {
-   margin: 40px;
-   width: 200px;
- }
- .pdf-hover:hover {
-   position: relative;
-   background: #EBEBEB;
- }
- .delete {
-   position: absolute;
-   left: 10px;
-   top: 10px;
- }
+.pdf-list-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+}
+.pdf {
+  margin: 40px;
+  width: 200px;
+}
+.pdf-hover:hover {
+  position: relative;
+  background: #ebebeb;
+}
+.delete {
+  position: absolute;
+  left: 10px;
+  top: 10px;
+}
 </style>
