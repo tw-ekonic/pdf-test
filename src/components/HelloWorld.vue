d6893a7a04d13efed5d79d7467e6e47a732e85ed
Jens Plüddemann <jens@plueddemann.de>
2021-08-25 15:42:05 +0200

    rotation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="pdf-container">
-    <div class="pdf-list-container" v-if="$data.pdf">
-      <div v-for="i in $data.pages" :key="i">
-        <div v-if="!toBeDeleted.includes(i)" class="pdf-hover">
+    <div class="pdf-list-container" v-if="$data.pagesCount">
+      <div v-for="index in $data.pagesCount" :key="index">
+        <div v-if="!toBeDeleted.includes(index)" class="pdf-hover">
           <div class="controls">
-            <button v-on:click="hidePage(i)">Delete</button>
-            <button v-on:click="rotatePDF(i)">Rotate</button>
+            <button v-on:click="hidePage(index)">Delete</button>
+            <button v-on:click="rotateRight($event, index)">
+              Rotate right
+            </button>
+            <button v-on:click="rotateLeft($event, index)">Rotate left</button>
           </div>
           <div>
-            <pdf class="pdf" :src="$data.renderedPdf" :page="i"> </pdf>
+            <pdf
+              class="pdf"
+              :src="$data.renderedPdf"
+              :page="index"
+              data-rotation="0"
+            ></pdf>
           </div>
+          <div>Page: {{ index }}</div>
         </div>
       </div>
     </div>
     <button v-on:click="revertHide">Undo Changes</button>
-    <button v-on:click="removePages">Save Changes</button>
+    <button v-on:click="saveChanges">Save Changes</button>
   </div>
 </template>
 
@@ -27,32 +36,44 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      pdf: undefined,
-      renderedPdf: '',
-      pages: 0,
+      pdf: PDFDocument,
+      renderedPdf: Uint8Array,
+      pagesCount: Number,
       toBeDeleted: [],
+      toBeRotated: new Map(),
     };
   },
   async mounted() {
     const url = 'test.pdf';
     const arrayBuffer = await fetch(url).then((res) => res.arrayBuffer());
     const doc = await PDFDocument.load(arrayBuffer);
-
-    this.$data.pdf = doc;
-    this.$data.renderedPdf = await doc.save();
-    this.pages = doc.getPageCount();
+    this.pdf = doc;
+    this.renderedPdf = await doc.save();
+    this.pagesCount = doc.getPageCount();
   },
   methods: {
-    async removePages() {
+    async saveChanges() {
       this.toBeDeleted.forEach((index) => this.pdf.removePage(index - 1));
-      console.log(this.pdf.getPageCount());
       this.toBeDeleted = [];
+
+      console.log(this.toBeRotated);
+      this.toBeRotated.forEach((value, key) => {
+        key -= key - 1;
+        console.log(value);
+        console.log(key);
+        const currentRotation = parseInt(
+          this.pdf.getPage(key).getRotation().angle
+        );
+        this.pdf.getPage(key).setRotation(degrees(currentRotation + value));
+      });
+      this.toBeRotated = new Map();
+
       this.$nextTick(async () => {
         const rawPdf = await this.pdf.save();
         const doc = await PDFDocument.load(rawPdf);
         this.pdf = doc;
         this.renderedPdf = await doc.save();
-        this.pages = doc.getPageCount();
+        this.pagesCount = doc.getPageCount();
       });
     },
     hidePage(i) {
@@ -64,26 +85,28 @@ export default {
       const lastIndex = this.toBeDeleted.length - 1;
       this.toBeDeleted.splice(lastIndex, 1);
     },
-    rotatePDF(i) {
-      const page = this.pdf.getPage(i - 1);
-      const currentRotation = page.getRotation().angle;
-      if (currentRotation === 0) {
-        page.setRotation(degrees(90));
-      } else if (currentRotation === 90) {
-        page.setRotation(degrees(180));
-      } else if (currentRotation === 180) {
-        page.setRotation(degrees(270));
-      } else if (currentRotation === 270) {
-        page.setRotation(degrees(0));
-      }
+    rotateLeft(event, i) {
+      console.log(i);
+      const attribute = 'data-rotation';
+      const element = event.target.parentNode.nextSibling.firstChild;
+      const rotation = parseInt(element.getAttribute(attribute));
+      const newRotation = (rotation - 90) % 360;
 
-      this.$nextTick(async () => {
-        /*        const rawPdf = await this.pdf.save();*/
-        /*        const doc = await PDFDocument.load(rawPdf);
-        this.pdf = doc;*/
-        this.renderedPdf = await this.pdf.save();
-        /*        this.pages = doc.getPageCount();*/
-      });
+      element.setAttribute(attribute, newRotation);
+      element.style.transform = `rotate(${newRotation}deg)`;
+
+      this.toBeRotated.set(i, newRotation);
+    },
+    rotateRight(event, i) {
+      console.log(i);
+      const attribute = 'data-rotation';
+      const element = event.target.parentNode.nextSibling.firstChild;
+      const rotation = parseInt(element.getAttribute(attribute));
+      const newRotation = (rotation + 90) % 360;
+
+      element.setAttribute(attribute, newRotation);
+      element.style.transform = `rotate(${newRotation}deg)`;
+      this.toBeRotated.set(i, newRotation);
     },
   },
   components: {
